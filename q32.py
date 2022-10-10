fd5c104dd6cb3012820a122787d43cf0a76ad75d
PYO127 <eunpyokim127@gmail.com>
2022-10-10 23:23:12 +0900

    [Ch12] 그래프 Q32

--- q32.py
+++ q32.py
@@ -1,24 +1,26 @@
 # p331 Q32. 섬의 개수
+# https://leetcode.com/problems/number-of-islands/
 from typing import List
 
+
 class Solution:
-    def numIsland(self, grid:List[List[str]]) -> int:
-        def dfs(i, j): # 중첩함수
-            if i<0 or i>= len(grid) or \
-                j<0 or j>= len(grid[0]) or \
-                grid[i][j] != '1':
-                return
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y):
+            grid[x][y] = 0
+            if x + 1 < len(grid[0]) and grid[x+1][y] == '1':
+                dfs(x + 1, y)
+            if x > 0 and grid[x-1][y] == '1':
+                dfs(x - 1, y)
+            if y + 1 < len(grid) and grid[x][y+1] == '1':
+                dfs(x, y + 1)
+            if y > 0 and grid[x][y-1] == '1':
+                dfs(x, y - 1)
 
-            grid[i][j] = '0'
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-
-        count = 0
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    dfs(i,j)
-                    count += 1
-        return count
+                    ans += 1
+                    dfs(i, j)
+
+        return ans
