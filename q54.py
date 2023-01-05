cbdb2bef238711e58a6e3dc7caa7cfaad8e05a46
PYO <64001061+PYO127@users.noreply.github.com>
2023-01-05 23:10:14 +0900

    Update q54.py
    필요없는 부분 제거

--- q54.py
+++ q54.py
@@ -13,12 +13,3 @@ class Solution:
             root.right = self.buildTree(preorder, inorder[index+1:]) # index+1 부터 끝까지
 
             return root
-
-
-S = Solution()
-preorder = [3,9,20,15,7]
-inorder = [9,3,15,20,7]
-
-S.buildTree(preorder, inorder)
-
-print()
