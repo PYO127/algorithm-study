f020b57277e99ca0e35be7f2b106a8a99fc78e07
PYO127 <eunpyokim127@gmail.com>
2023-02-02 22:54:22 +0900

    BOJ1806 부분합 (left > right 처리 추가)

--- learn-and-run/boj1806.py
+++ learn-and-run/boj1806.py
@@ -14,6 +14,13 @@ while left <= right < n:
             ans = right - left + 1
         curr_sum -= seq[left]
         left += 1
+        # 만약 left = right, seq[left] >= curr_sum 이 되면
+        # while 위에서 left +=1 이 되고, while문 밖으로
+        # 나가진다. 정답에는 이상이 없을 것 같지만(정답이 될 수 있는
+        # 최소 길이 = 1), 의도한 로직은 그게 아니므로 아래에
+        # left 가 right 보다 커졌을 경우를 처리해준다.
+        if left > right:
+            right = left
     else:
         right += 1
         if right < n:
@@ -23,3 +30,4 @@ if ans == n+1:
     print(0)
 else:
     print(ans)
+
