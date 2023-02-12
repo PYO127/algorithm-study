ab3b59fa2d9fc1582a3b59c6eb20c8d38442b593
PYO127 <eunpyokim127@gmail.com>
2023-02-12 15:30:04 +0900

    BOJ1695 숨바꼭질

---
+++ learn-and-run/boj1697.py
@@ -0,0 +1,28 @@
+# boj.kr/1697 숨바꼭질
+import collections
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+cost = [-1] * 100001
+deque = collections.deque()
+
+cost[n] = 0
+deque.append(n)
+
+while len(deque) != 0:
+    x = deque.popleft()
+    t = cost[x]
+    if 2 * x < 100001 and cost[2 * x] == -1:
+        deque.append(2 * x)
+        cost[2 * x] = t + 1
+    if x + 1 < 100001 and cost[x + 1] == -1:
+        deque.append(x + 1)
+        cost[x + 1] = t + 1
+    if x - 1 > -1 and cost[x - 1] == -1:
+        deque.append(x - 1)
+        cost[x - 1] = t + 1
+    if cost[k] != -1:
+        break
+
+print(cost[k])
