ac9f364f24c98ae989521d5df0bc242cf641ad9a
PYO127 <eunpyokim127@gmail.com>
2023-02-19 13:41:28 +0900

    [Ch17] 삽입 정렬 리스트 Q60 - 시간 개선

--- algorithm-interview/q60.py
+++ algorithm-interview/q60.py
@@ -11,6 +11,9 @@ class Solution:
 
             # 다중할당: 우변에 기존 값을 가지고 있고, 좌변에 차례대로 값을 바인딩.
             curr.next, head.next, head = head, curr.next, head.next
-            curr = parent
+
+            # 필요한 경우에만 curr가 처음(parent)으로 돌아가도록 처리
+            if head and head.val < curr.next.val:
+                curr = parent
 
         return parent.next
